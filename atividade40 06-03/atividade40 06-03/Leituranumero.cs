41843fd4968506aa988a0a9d3dbbabde5e646397
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2024-04-12 15:30:18 -0300

    Add files via upload

---
+++ atividade40 06-03/atividade40 06-03/Leituranumero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace exercicio40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = int.MinValue; 
+            int menor = int.MaxValue;
+            
+                    Console.Write("Digite um número inteiro: ");
+                    int valor = int.Parse(Console.ReadLine());
+                {
+                while (valor > 0)
+                {
+                    if (valor < 0)
+
+
+                        if (valor > maior)
+                            maior = valor;
+
+                    if (valor < menor)
+                        menor = valor;
+                }
+                }
+
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro válido.");
+                }
+            
+
+            Console.WriteLine($"O maior número lido foi: {maior}");
+            Console.WriteLine($"O menor número lido foi: {menor}");
+        }
+    }
+}
+
